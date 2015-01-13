81ff59c087560fa1145257518b149959cbc89ed6
ggouzi <gaetan.gouzi@insa-lyon.fr>
2015-01-13 21:33:12 +0100

    Ajout de toutes les classes

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -48,7 +48,7 @@ Circle::Circle ( const Circle & unCircle )
 } //----- Fin de Circle (constructeur de copie)
 
 
-Circle::Circle ( )
+Circle::Circle (long newRadius, Point figureOrigin, string graphicsName, string graphicsCommandLine)
 // Algorithme :
 //
 {
@@ -71,5 +71,41 @@ Circle::~Circle ( )
 //------------------------------------------------------------------ PRIVE
 
 //----------------------------------------------------- Méthodes protégées
+Circle::bool isInside(Point p1, Point p2)
+// Algorithme :
+//
+{
 
+	if( (p1.x <=origin.x-radius) &&
+		(p2.x >=origin.x+radius) &&
+		(p1.y >=origin.y+radius) &&
+		(p2.y <=origin.y-radius) )
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
+} //----- Fin de isInside
+
+Circle::virtual string description()
+// Algorithme :
+//
+{
+	return commandLine;
+
+} //----- Fin de description
+
+Circle::virtual void move(long dx, long dy)
+// Algorithme :
+//
+{
+	origin.x += dx;
+	origin.y += dy;
+
+	commandLine="C "+name+" "+Long_to_string(origin.x)+" "+Long_to_string(origin.y)+" "+Long_to_string(radius);
+
+} //----- Fin de move
 //------------------------------------------------------- Méthodes privées
